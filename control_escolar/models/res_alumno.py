1cbfe669245c1ac909aaf51b61e2f2f1f495867b
Marco Antonio Navarro Camarena <marcoanc95@gmail.com>
2021-05-24 16:37:56 -0500

    2

--- control_escolar/models/res_alumno.py
+++ control_escolar/models/res_alumno.py
@@ -8,4 +8,12 @@ class ResAlumno(models.Model):
      name = fields.Char(string='Nombre', copy=False, index=True, default=lambda self: _('New'))
      edad = fields.Integer(string="Edad")
      promedio = fields.Float(string="Promedio")
+
+     @api.model
+     def create(self, vals):
+          if vals.get('name', _('New')) == _('New'):
+               vals['name'] = self.env['ir.sequence'].next_by_code('installed.services') or _('New')
+
+          result = super(ResAlumno, self).create(vals)
+          return result
      
