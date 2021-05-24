32a29c5fcb5664b22a0ea5cbb96626b1f99e0ca7
Marco Antonio Navarro Camarena <marcoanc95@gmail.com>
2021-05-24 16:34:15 -0500

    [FIX] 5

--- control_escolar/models/res_alumno.py
+++ control_escolar/models/res_alumno.py
@@ -5,7 +5,7 @@ class ResAlumno(models.Model):
      _name = 'res.alumno'
      _inherit = ['mail.thread', 'mail.activity.mixin']
 
-     name = fields.Char(string="Nombre")
+     name = fields.Char(string='Nombre', copy=False, index=True, default=lambda self: _('New'))
      edad = fields.Integer(string="Edad")
      promedio = fields.Float(string="Promedio")
      
